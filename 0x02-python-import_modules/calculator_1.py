8142be03ecf65e7fe881590b9045afe6b3986a94
Emmyraay <ibrahim.emmy@yahoo.com>
2022-09-23 03:05:59 -0700

    Task 1

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Adds two numbers"""
+    return a + b
+
+
+def sub(a, b):
+    """subtracts two numbers"""
+    return a - b
+
+
+def mul(a, b):
+    """multiplies two numbers"""
+    return a * b
+
+
+def div(a, b):
+    """divides two numbers"""
+    return int(a / b)
